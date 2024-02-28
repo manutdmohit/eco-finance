c7b0b538ac6b6e424b07c1cff14ba05001a4fa9a
Mohit Saud <mohit@prabidhilabs.com>
2024-02-28 16:39:27 +0545

    Remove Loan Calculator

--- app/components/Wrapper/wrapper.tsx
+++ app/components/Wrapper/wrapper.tsx
@@ -390,12 +390,12 @@ export const Wrapper = () => {
             {/* <!-- logo --> */}
             <div className="col-md-9 text-right d-flex justify-content-end align-items-center">
               <div className="header-action ">
-                <a
+                {/* <a
                   href="/loancalculator/"
                   className="btn btn-primary header-button loan-calc"
                 >
                   Loan Calculators
-                </a>{' '}
+                </a> */}
                 <a
                   href="/contact-us/"
                   className="btn btn-default header-button call-now"
