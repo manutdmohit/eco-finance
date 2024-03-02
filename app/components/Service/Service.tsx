9093d6a97ca85fcbb7bf3a2eefe3a5ffcd0495a7
Mohit Saud <mohit@prabidhilabs.com>
2024-03-02 10:23:21 +0545

    Update services title
    - Add Loans at the end of each title

--- app/components/Service/Service.tsx
+++ app/components/Service/Service.tsx
@@ -24,38 +24,38 @@ const Service: React.FC = () => {
             />
             <ServiceItem
               image="./assets/investment-loans.jpg"
-              title="Investment"
+              title="Investment Loans"
               description="Discover investment opportunities and grow your wealth with our investment services."
             />
             <ServiceItem
               image="./assets/refinance-loans.jpg"
-              title="Refinance"
+              title="Refinance Loans"
               description="Refinance your existing mortgage to lower your monthly payments or take advantage of lower interest rates."
             />
             <ServiceItem
               image="./assets/construction-loans.jpg
           "
-              title="Construction"
+              title="Construction Loans "
               description="Secure financing for your construction project and turn your dream home into a reality."
             />
             <ServiceItem
               image="./assets/business-loans.jpg"
-              title="Business"
+              title="Business Loans"
               description="Explore financing options for your business and fuel its growth and expansion."
             />
             <ServiceItem
               image="./assets/commerical-loans.jpg"
-              title="Commercial"
+              title="Commercial Loans"
               description="Obtain financing for commercial properties, real estate projects, and more."
             />
             <ServiceItem
               image="./assets/personal-loans.jpg"
-              title="Personal"
+              title="Personal Loans"
               description="Get personalized financial advice and solutions tailored to your individual needs and goals."
             />
             <ServiceItem
               image="./assets/car-loans.jpg"
-              title="Car"
+              title="Car Loans"
               description="Finance your next car purchase with our competitive auto loan options."
             />
           </Row>
