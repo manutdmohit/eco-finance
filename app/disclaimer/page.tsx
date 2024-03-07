c5e0f72236523c9fdcacca08e6030c8c2d9b091a
Mohit Saud <mohit@prabidhilabs.com>
2024-03-07 14:41:28 +0545

    Add Disclaimer page

---
+++ app/disclaimer/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import Footer from '../components/Footer/Footer';
+import TopBar from '../components/Top/top';
+import Wrapper from '../components/Wrapper/wrapper';
+
+const Disclaimer = () => {
+  return (
+    <div>
+      <TopBar />
+      <Wrapper />
+      <Container className="disclaimer-container mt-5">
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <Card>
+              <Card.Header>
+                <h2>Disclaimer</h2>
+              </Card.Header>
+              <Card.Body>
+                <p className="text-black">
+                  The information provided on this site is on the understanding
+                  that it is for illustrative and discussion purposes only.
+                  Whilst all care and attention is taken in its preparation any
+                  party seeking to rely on its content or otherwise should make
+                  their own enquiries and research to ensure its relevance to
+                  your specific personal and business requirements and
+                  circumstances. Terms, conditions, fees and charges may apply.
+                  Normal lending criteria apply. Rates subject to change.
+                  Approved applicants only.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default Disclaimer;
