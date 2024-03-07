dc1168ed9d41fc11b2e658158b6b136270bcd560
Mohit Saud <mohit@prabidhilabs.com>
2024-03-07 15:18:45 +0545

    Add Home Page - Added TopBar, Wrapper, Introduction, Assistance Section, and Footer

---
+++ app/home/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Footer from '../components/Footer/Footer';
+import TopBar from '../components/Top/top';
+import Wrapper from '../components/Wrapper/wrapper';
+import Introduction from '../components/Introduction/Introduction';
+import AssistanceSection from '../components/Assist/Assist';
+
+const Home = () => {
+  return (
+    <div>
+      <TopBar />
+      <Wrapper />
+      <Introduction />
+      <AssistanceSection />
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
