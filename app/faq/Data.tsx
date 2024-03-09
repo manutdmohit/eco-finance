7a6bf8d2742d9bb254324bd57f610bcd3fd9a91f
Mohit Saud <mohit@prabidhilabs.com>
2024-03-09 12:01:04 +0545

    Add FAQ section

---
+++ app/faq/Data.tsx
@@ -0,0 +1,19 @@
+const data = [
+  {
+    question: 'What income documents will I need for a home loan?',
+    answer:
+      "For full-time PAYG, you can usually just provide your latest 2 payslips. For casual, you'll sometimes also need your last year's PAYG summary. Self-employed applicants will usually need to provide the full 2 years' financials: personal tax returns, Notice of Assessments, company tax returns, company profit & loss + balance sheet.",
+  },
+  {
+    question: 'How long does it take for a mortgage to be approved?',
+    answer:
+      'The time it takes for a mortgage application varies based on factors like application complexity and the lender. Simple applications with responsive lenders can be quick, while complex ones or slower lenders may take longer. Typically, it is approved within 14 days, sometimes even sooner if time is limited. We will provide an estimated timeline upon submission and keep you informed throughout. In general, most applications are settled within a month, often sooner.',
+  },
+  {
+    question: 'Is it better to apply for a mortgage through a broker?',
+    answer:
+      'Using a mortgage broker for your home loan has numerous benefits with no downside. We provide personalized care and access to top loan products from a wide range of lenders. In case of complex applications, our expertise ensures a thorough assessment and finding the ideal loan for you',
+  },
+];
+
+export default data;
