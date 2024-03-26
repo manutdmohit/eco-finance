c2fded6eccb33f035941e771a2998ea85f0dca4f
Mohit Saud <mohit@prabidhilabs.com>
2024-03-26 10:31:11 +0545

    Fix:: services sub-menu not showing - Update a tag with Link

--- app/components/Wrapper/wrapper.tsx
+++ app/components/Wrapper/wrapper.tsx
@@ -130,20 +130,20 @@ export const Navigation = () => {
                     className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-home mega-current-menu-item mega-page_item mega-page-item-1611 mega-current_page_item mega-align-bottom-left mega-menu-grid mega-menu-item-1626"
                     id="mega-menu-item-1626"
                   >
-                    <a
+                    <Link
                       className="mega-menu-link animsition-link"
                       href="/home"
                       aria-current="page"
                     >
                       Home
-                    </a>
+                    </Link>
                   </li>
                   <li
                     className="mega-menu-item mega-menu-item-type-custom mega-menu-item-object-custom mega-menu-item-has-children mega-align-bottom-left mega-menu-flyout mega-menu-item-6603 has-sub"
                     id="mega-menu-item-6603"
                   >
                     <span className="submenu-button"></span>
-                    <a
+                    <Link
                       className="mega-menu-link animsition-link"
                       href="/services"
                       aria-haspopup="true"
@@ -154,181 +154,90 @@ export const Navigation = () => {
                         className="mega-indicator"
                         data-has-click-event="true"
                       ></span>
-                    </a>
+                    </Link>
                     <ul className="mega-sub-menu">
                       <li
-                        className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-6604 w-100"
+                        className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-6604"
                         id="mega-menu-item-6604"
                       >
-                        <a
-                          className="mega-menu-link animsition-link"
+                        <Link
+                          className="mega-menu-link animsition-link w-100"
                           href="/smsf-property-investment/"
                         >
                           Home Loans & Investment Loans
-                        </a>
+                        </Link>
                       </li>
                       <li
                         className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-6604"
                         id="mega-menu-item-6604"
                       >
-                        <a
+                        <Link
                           className="mega-menu-link animsition-link"
                           href="/smsf-property-investment/"
                         >
                           Refinance & Construction Loans
-                        </a>
+                        </Link>
                       </li>
                       <li
                         className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-6604"
                         id="mega-menu-item-6604"
                       >
-                        <a
+                        <Link
                           className="mega-menu-link animsition-link"
                           href="/smsf-property-investment/"
                         >
                           Business & Commercial Loans
-                        </a>
+                        </Link>
                       </li>
                       <li
                         className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-6604"
                         id="mega-menu-item-6604"
                       >
-                        <a
+                        <Link
                           className="mega-menu-link animsition-link"
                           href="/smsf-property-investment/"
                         >
                           Personal & Car Loans
-                        </a>
+                        </Link>
                       </li>
                       <li
                         className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-6604"
                         id="mega-menu-item-6604"
                       >
-                        <a
+                        <Link
                           className="mega-menu-link animsition-link"
                           href="/smsf-home-loans"
                         >
                           SMSF Home Loans
-                        </a>
+                        </Link>
                       </li>
                     </ul>
                   </li>
-                  {/* <li
-                    className="mega-menu-item mega-menu-item-type-custom mega-menu-item-object-custom mega-menu-item-has-children mega-align-bottom-left mega-menu-flyout mega-menu-item-6603 has-sub"
-                    id="mega-menu-item-6603"
-                  >
-                    <span className="submenu-button"></span>
-                    <a
-                      className="mega-menu-link animsition-link"
-                      href="/smsf-property-investment/"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      SMSF
-                      <span
-                        className="mega-indicator"
-                        data-has-click-event="true"
-                      ></span>
-                    </a>
-                    <ul className="mega-sub-menu">
-                      <li
-                        className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-6604"
-                        id="mega-menu-item-6604"
-                      >
-                        <a
-                          className="mega-menu-link animsition-link"
-                          href="/smsf-property-investment/"
-                        >
-                          SMSF Property Investment
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li
-                    className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-align-bottom-left mega-menu-flyout mega-menu-item-1792"
-                    id="mega-menu-item-1792"
-                  >
-                    <a
-                      className="mega-menu-link animsition-link"
-                      href="/resources/"
-                    >
-                      Resources
-                    </a>
-                  </li>
-                  <li
-                    className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-has-children mega-align-bottom-left mega-menu-flyout mega-menu-item-1653 has-sub"
-                    id="mega-menu-item-1653"
-                  >
-                    <span className="submenu-button"></span>
-                    <a
-                      className="mega-menu-link animsition-link"
-                      href="/loancalculator/"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      Loan Calculators
-                      <span
-                        className="mega-indicator"
-                        data-has-click-event="true"
-                      ></span>
-                    </a>
-                    <ul className="mega-sub-menu">
-                      <li
-                        className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-1479"
-                        id="mega-menu-item-1479"
-                      >
-                        <a
-                          className="mega-menu-link animsition-link"
-                          href="/monthly-repayment-calculator/"
-                        >
-                          EMI Calculator
-                        </a>
-                      </li>
-                      <li
-                        className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-1506"
-                        id="mega-menu-item-1506"
-                      >
-                        <a
-                          className="mega-menu-link animsition-link"
-                          href="/loan-eligibility-calculator/"
-                        >
-                          Borrowing Power Calculator
-                        </a>
-                      </li>
-                    </ul>
-                  </li> */}
-                  {/* <li
-                    className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-align-bottom-left mega-menu-flyout mega-menu-item-1483"
-                    id="mega-menu-item-1483"
-                  >
-                    <a className="mega-menu-link animsition-link" href="/blog/">
-                      Blog
-                    </a>
-                  </li> */}
+
                   <li
                     className="mega-menu-item mega-menu-item-type-custom mega-menu-item-object-custom mega-menu-item-has-children mega-align-bottom-left mega-menu-flyout mega-menu-item-6603 has-sub"
                     id="mega-menu-item-6603"
                   >
                     <span className="submenu-button"></span>
-                    <a
+                    <Link
                       className="mega-menu-link animsition-link"
                       href="/about-us/"
                       aria-haspopup="true"
                       aria-expanded="false"
                     >
                       About Us
-                    </a>
+                    </Link>
                   </li>
                   <li
                     className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-align-bottom-left mega-menu-flyout mega-menu-item-1486"
                     id="mega-menu-item-1486"
                   >
-                    <a
+                    <Link
                       className="mega-menu-link animsition-link"
                       href="/contact-us/"
                     >
                       Contact Us
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </div>{' '}
@@ -351,38 +260,38 @@ export const Navigation = () => {
 
 const Wrapper = () => {
   return (
-    <div id="sticky-wrapper" className="sticky-wrapper overflow-hidden">
+    <div id="sticky-wrapper" className="sticky-wrapper">
       <div className="header-2 sticky-header">
         <div className="container">
           <div className="row">
             <div className="col-md-2 col-sm-12 col-xs-6">
               {/* <!-- logo --> */}
               <div className="logo">
-                <a href="/">
+                <Link href="/">
                   <img
                     src="
                   https://res.cloudinary.com/manutdmohit/image/upload/ECO_FINANCE_LOGO_ryqruv.png
                   "
                     alt="Eco Finance And Home Loans"
                   />
-                </a>
+                </Link>
               </div>
             </div>
             {/* <!-- logo --> */}
             <div className="col-md-10 text-right d-flex justify-content-end align-items-center">
               <div className="header-action ">
-                <a
+                <Link
                   href="/calculators/"
                   className="btn btn-default header-button loan-calc"
                 >
                   Loan Calculators
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/contact-us/"
                   className="btn btn-default header-button call-now"
                 >
                   Get A Callback
-                </a>{' '}
+                </Link>{' '}
               </div>
             </div>
           </div>
